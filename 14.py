c1a39ae191a31e73aa13d08ee8d1fcccdeb85026
tigervanilla <piyushcool102@gmail.com>
2020-04-20 22:59:41 +0530

    Problem 14 explaination

--- 14.py
+++ 14.py
@@ -23,3 +23,19 @@ def estimate_PI_using_monte_carlo():
     return round(pi, 3)
 
 print('Pi =', estimate_PI_using_monte_carlo())
+
+
+# EXPLANATION
+# Draw a square of side 1 unit centered at (0,0)
+# Draw a circle inside of radius 0.5 inside the square
+# The circle will be touching the square’s sides but there will be a part of the square that does not overlap with the circle
+
+# Area of circle / Area of square  = Pi / 4
+
+# Now plot random points inside the square ie (-0.5 <= X <= 0.5) and (-0.5 <= Y <= 0.5)
+# Count the number of points inside the circle and total points
+# Since the equation of circle is given by X^2 + Y^2 = R^2,
+# therefore a point(X,Y) is inside a circle if X^2 + Y^2 <= R^2
+
+# Number of points inside circle / Total number of points = Pi / 4
+# => Pi = 4 * (Number of points inside circle / Total number of points)
