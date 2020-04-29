8811b924183cb78a6af8aac6e27d87fe5e6dd48e
tigervanilla <piyushcool102@gmail.com>
2020-04-29 16:04:22 +0530

    Extenden Euclidean Algo - Explanation

--- extended_euclidean_algo.py
+++ extended_euclidean_algo.py
@@ -9,3 +9,18 @@ def GCD_extendedEuclidean(a, b):
 
 # Driver code:
 print('gcd=%d, x=%d, y=%d' % GCD_extendedEuclidean(7, 5))
+
+
+# Explanation:
+# The extended euclidean algorithm says that
+# GCD(a, b) = ax + by         ----(1)
+
+# We know that GCD(a, b) = GCD(b, a % b)
+# GCD(a, b) = GCD(b, a % b)
+#           = b*x1 + (a % b)*y1
+#           = b*x1 + (a - floor(a/b)*b)*y1
+#           = a*y1 + (x1 - floor(a/b)*y1)       -----(2)
+
+# From (1) and (2):
+# x = y1
+# y = x1 - floor(a/b)*y1
