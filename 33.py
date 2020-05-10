316e6f783f1e9fa61f44df3e59e1f5d0aaff17b1
tigervanilla <piyushcool102@gmail.com>
2020-05-10 20:02:31 +0530

    Problem 33: Running Length Median - Heap approach

--- 33.py
+++ 33.py
@@ -9,6 +9,7 @@
 
 # [2, 1.5, 2, 3.5, 2, 2, 2]
 
+
 def insertionSort(arr, n):
     '''arr is already sorted, except the last element'''
     temp = arr[n - 1]
@@ -32,6 +33,127 @@ def runningMedian(stream):
             print((streamSoFar[cnt // 2] + streamSoFar[cnt // 2 - 1]) * 0.5)
 
 
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def size(self):
+        return len(self.heap)
+
+    def insert(self, val):
+        self.heap.append(val)
+        i = self.size() - 1
+        while i > 0:
+            parent = (i - 1) // 2
+            if val > self.heap[parent]:
+                return
+            self.heap[i] = self.heap[parent]
+            self.heap[parent] = val
+            i = parent
+
+    def pop(self):
+        ans = self.peek()
+        if self.size() > 1:
+            self.heap[0] = self.heap.pop()
+            i, n = 0, self.size() - 1
+            while i < n:
+                left, right, smallest = 2*i + 1, 2*i + 2, i
+                if left <= n and self.heap[left] < self.heap[smallest]:
+                    smallest = left
+                if right <= n and self.heap[right] < self.heap[smallest]:
+                    smallest = right
+                if i == smallest:
+                    break
+                self.heap[i], self.heap[smallest] = self.heap[smallest], self.heap[i]
+                i = smallest
+        return ans
+
+    def peek(self):
+        return self.heap[0]
+
+    def display(self):
+        print(self.heap)
+
+
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def size(self):
+        return len(self.heap)
+
+    def insert(self, val):
+        self.heap.append(val)
+        i = self.size() - 1
+        while i > 0:
+            parent = (i-1) // 2
+            if val <= self.heap[parent]:
+                return
+            self.heap[i] = self.heap[parent]
+            self.heap[parent] = val
+            i = parent
+
+    def pop(self):
+        ans = self.peek()
+        if self.size() > 1:
+            self.heap[0] = self.heap.pop()
+            i, n = 0, self.size() - 1
+            while i < n:
+                left, right, largest = 2*i + 1, 2*i + 2, i
+                if left <= n and self.heap[left] > self.heap[largest]:
+                    largest = left
+                if right <= n and self.heap[right] > self.heap[largest]:
+                    largest = right
+                if i == largest:
+                    break
+                self.heap[i], self.heap[largest] = self.heap[largest], self.heap[i]
+                i = largest
+        return ans
+
+    def peek(self):
+        return self.heap[0]
+
+    def display(self):
+        print(self.heap)
+
+
+def runningMedian2_helper(item, medianSoFar, leftMaxheap, rightMinheap):
+    if leftMaxheap.size() == rightMinheap.size():
+        if item < medianSoFar:
+            leftMaxheap.insert(item)
+            return leftMaxheap.peek()
+        else:
+            rightMinheap.insert(item)
+            return rightMinheap.peek()
+    elif leftMaxheap.size() > rightMinheap.size():
+        if item < medianSoFar:
+            rightMinheap.insert(leftMaxheap.pop())
+            leftMaxheap.insert(item)
+        else:
+            rightMinheap.insert(item)
+        return (leftMaxheap.peek() + rightMinheap.peek()) / 2
+    else:
+        if item < medianSoFar:
+            leftMaxheap.insert(item)
+        else:
+            leftMaxheap.insert(rightMinheap.pop())
+            rightMinheap.insert(item)
+        return (leftMaxheap.peek() + rightMinheap.peek()) / 2
+
+
+def runningMedian2(stream):
+    '''
+    Using minheap on left side of running median
+    and maxheap on right side of running median
+    '''
+    leftMaxheap, rightMinheap = MaxHeap(), MinHeap()
+    medianSoFar = 0
+    for item in stream:
+        medianSoFar = runningMedian2_helper(item, medianSoFar, leftMaxheap, rightMinheap)
+        print(medianSoFar)
+
+
 # Driver code:
 stream = [2, 1, 5, 7, 2, 0, 5]
 runningMedian(stream)
+runningMedian2(stream)
